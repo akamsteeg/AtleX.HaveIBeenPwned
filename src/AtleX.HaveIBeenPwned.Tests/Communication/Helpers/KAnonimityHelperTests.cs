c98428241e078a2b3ff1bbe833d0fd2add8aab35
Alex Kamsteeg <alex@atlex.nl>
2018-10-27 15:05:34 +0200

    Added unit tests for KAnonimityHelper

---
+++ src/AtleX.HaveIBeenPwned.Tests/Communication/Helpers/KAnonimityHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using AtleX.HaveIBeenPwned.Communication.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AtleX.HaveIBeenPwned.Tests.Communication.Helpers
+{
+  public class KAnonimityHelperTests
+  {
+    [Fact]
+    public void GetHashForPassword_WithEmptyPassword_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() => KAnonimityHelper.GetHashForPassword(""));
+    }
+
+    [Fact]
+    public void GetHashForPassword_WithPassword_GeneratesValidHash()
+    {
+      var hash = KAnonimityHelper.GetHashForPassword("DUMMY");
+
+      Assert.NotNull(hash);
+      Assert.Equal(40, hash.Length);
+    }
+  }
+}
