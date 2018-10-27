881627259883570021c5d8bf1cfaf029336b1327
Alex Kamsteeg <alex@atlex.nl>
2018-10-27 14:59:19 +0200

    Added unit tests for HaveIBeenPwnedClientException

---
+++ src/AtleX.HaveIBeenPwned.Tests/Communication/HaveIBeenPwnedClientExceptionTests.cs
@@ -0,0 +1,41 @@
+﻿using AtleX.HaveIBeenPwned.Communication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AtleX.HaveIBeenPwned.Tests.Communication
+{
+  public class HaveIBeenPwnedClientExceptionTests
+  {
+    [Fact]
+    public void Ctor_WithNoParams_DoesNotThrow()
+    {
+      var e = new HaveIBeenPwnedClientException();
+    }
+
+    [Fact]
+    public void Ctor_WithEmptyMessage_DoesNotThrow()
+    {
+      var e = new HaveIBeenPwnedClientException("");
+    }
+
+    [Fact]
+    public void Ctor_WithMessage_DoesNotThrow()
+    {
+      var e = new HaveIBeenPwnedClientException("TEST");
+    }
+
+    [Fact]
+    public void Ctor_WithMessageAndNullInnerException_DoesNotThrow()
+    {
+      var e = new HaveIBeenPwnedClientException("TEST", null);
+    }
+
+    [Fact]
+    public void Ctor_WithMessageAndInnerException_DoesNotThrow()
+    {
+      var e = new HaveIBeenPwnedClientException("TEST", new Exception());
+    }
+  }
+}
