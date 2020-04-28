e2214ecdd9ddaababaf486ab5e467c0e9e6c74c7
Alex Kamsteeg <alex@atlex.nl>
2020-04-28 19:16:10 +0200

    Cleaned up benchmark config and enabled usage of CLI arguments

--- src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
+++ src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
@@ -11,20 +11,22 @@ namespace AtleX.HaveIBeenPwned.Benchmarks
     private static void Main(string[] args)
     {
       var config = GetConfig();
-      BenchmarkRunner.Run(Assembly.GetExecutingAssembly(), config);
+      BenchmarkSwitcher
+        .FromAssembly(Assembly.GetExecutingAssembly())
+        .Run(args, config);
     }
 
     private static IConfig GetConfig()
     {
       var config = ManualConfig.Create(DefaultConfig.Instance);
 
-      config.AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default);
+      config
+        .AddDiagnoser(BenchmarkDotNet.Diagnosers.MemoryDiagnoser.Default);
 
       config.AddJob(
         Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp31).AsBaseline(),
         Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp21),
-        Job.Default.WithToolchain(CsProjClassicNetToolchain.Net472),
-        Job.Default.WithToolchain(CsProjClassicNetToolchain.Net461)
+        Job.Default.WithToolchain(CsProjClassicNetToolchain.Net472)
         );
 
       return config;
