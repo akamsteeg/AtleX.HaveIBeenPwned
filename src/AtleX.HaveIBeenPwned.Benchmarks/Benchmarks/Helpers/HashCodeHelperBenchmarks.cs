cc7d0cbe0e1e9ef6b5964df033ed65d9eacdd37b
Alex Kamsteeg <alex@atlex.nl>
2020-06-27 14:53:41 +0200

    Add benchmarks for HashCodeHelper

---
+++ src/AtleX.HaveIBeenPwned.Benchmarks/Benchmarks/Helpers/HashCodeHelperBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using AtleX.HaveIBeenPwned.Helpers;
+using BenchmarkDotNet.Attributes;
+
+namespace AtleX.HaveIBeenPwned.Benchmarks.Benchmarks.Helpers
+{
+  public class HashCodeHelperBenchmarks
+  {
+    [Benchmark]
+    [Arguments("-&Hx")]
+    [Arguments("-&HxcB_d")]
+    [Arguments("-&HxcB_dH+M@BZAX")]
+    [Arguments("-&HxcB_dH+M@BZAXHCt7eJv.)eJ&,#x}")]
+    public int GetHashCode(string value)
+    {
+      var result = HashCodeHelper.GetHashCode(value);
+
+      return result;
+    }
+
+    [Benchmark]
+    [Arguments("-&Hx", "-&HxcB_dH+M@BZAXHCt7eJv.)eJ&,#x}")]
+    [Arguments("-&HxcB_d", "-&Hx")]
+    [Arguments("-&HxcB_dH+M@BZAX", "-&HxcB_d")]
+    [Arguments("-&HxcB_dH+M@BZAXHCt7eJv.)eJ&,#x}", "-&HxcB_dH+M@BZAX")]
+    public int GetHashCode(string value1, string value2)
+    {
+      var result = HashCodeHelper.GetHashCode(value1, value2);
+
+      return result;
+    }
+  }
+}
