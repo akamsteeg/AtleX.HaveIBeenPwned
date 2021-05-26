b622b9bc2b38e0f11b2786ec3b9d51e9202bd474
Alex Kamsteeg <alex@atlex.nl>
2021-05-26 17:12:55 +0200

    Added benchmarks for UriFactory

---
+++ src/AtleX.HaveIBeenPwned.Benchmarks/Benchmarks/UriFactoryBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace AtleX.HaveIBeenPwned.Benchmarks.Benchmarks
+{
+  public class UriFactoryBenchmarks
+  {
+    private const string Account = "mail@example.com";
+
+    [Benchmark]
+    public Uri GetAllBreachesUri()
+    {
+      return UriFactory.GetAllBreachesUri();
+    }
+
+    [Benchmark]
+    [Arguments(BreachMode.All)]
+    [Arguments(BreachMode.ExcludeUnverified)]
+    public Uri GetBreachesForAccountUri(BreachMode breachMode)
+    {
+      return UriFactory.GetBreachesForAccountUri(Account, breachMode);
+    }
+
+    [Benchmark]
+    public Uri GetPasteAccountUri()
+    {
+      return UriFactory.GetPasteAccountUri(Account);
+    }
+
+    [Benchmark]
+    public Uri GetPwnedPasswordUri()
+    {
+      return UriFactory.GetPwnedPasswordUri("5BAA6");
+    }
+  }
+}
