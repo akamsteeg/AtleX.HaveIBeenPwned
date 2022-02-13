57aa1ec4c3a71147d1f8310d6f76092368ff1f1e
Alex Kamsteeg <alex@atlex.nl>
2022-02-13 12:40:40 +0100

    Remove obsolete obsolete properties from HaveIBeenPwnedClientSettings (fixes #60)

--- src/AtleX.HaveIBeenPwned/HaveIBeenPwnedClientSettings.cs
+++ src/AtleX.HaveIBeenPwned/HaveIBeenPwnedClientSettings.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace AtleX.HaveIBeenPwned;
 
@@ -13,17 +12,6 @@ public class HaveIBeenPwnedClientSettings
   /// Gets or sets the application name
   /// </summary>
   public string? ApplicationName
-  {
-    get;
-    set;
-  }
-
-  /// <summary>
-  /// Gets or sets the <see cref="TimeSpan"/> as timeout when communicating
-  /// with the HaveIBeenPwned.com service (defaults to 10 seconds)
-  /// </summary>
-  [Obsolete("The timeout must now be configured on the HTTP client itself")]
-  public TimeSpan TimeOut
   {
     get;
     set;
@@ -54,10 +42,4 @@ public class HaveIBeenPwnedClientSettings
     get;
     set;
   }
-
-  /// <summary>
-  /// Gets the default <see cref="HaveIBeenPwnedClientSettings"/>
-  /// </summary>
-  [Obsolete("Instantiate HaveIBeenPwnedClientSettings and provide values for the required properties", error: true)]
-  public static HaveIBeenPwnedClientSettings Default => throw new NotImplementedException("HaveIBeenPwnedClientSettings.Default is obsolete and must no longer be used");
 }
