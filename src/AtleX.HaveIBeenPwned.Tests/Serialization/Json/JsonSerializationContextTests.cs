e092bf16678f82fda8011d179bb53a03f09f2325
Alex Kamsteeg <alex@atlex.nl>
2022-10-29 10:00:34 +0200

    Added unit tests for JSON serialization factories and contexts

---
+++ src/AtleX.HaveIBeenPwned.Tests/Serialization/Json/JsonSerializationContextTests.cs
@@ -0,0 +1,28 @@
+﻿#if NET6_0_OR_GREATER
+
+using AtleX.HaveIBeenPwned.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AtleX.HaveIBeenPwned.Tests.Serialization.Json;
+
+public class JsonSerializationContextTests
+{
+  [Theory]
+  [InlineData(typeof(Breach))]
+  [InlineData(typeof(IEnumerable<Breach>))]
+  [InlineData(typeof(Paste))]
+  [InlineData(typeof(IEnumerable<Paste>))]
+  [InlineData(typeof(SiteBreach))]
+  [InlineData(typeof(IEnumerable<SiteBreach>))]
+  public void Context_SupportsType(Type type)
+  {
+    var c = new JsonSerializationContext();
+
+    var ti = c.GetTypeInfo(type);
+
+    Assert.NotNull(ti);
+  }
+}
+#endif
---
+++ src/AtleX.HaveIBeenPwned.Tests/Serialization/Json/JsonSerializerOptionsFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using AtleX.HaveIBeenPwned.Serialization.Json;
+using System.Text.Json;
+using Xunit;
+
+namespace AtleX.HaveIBeenPwned.Tests.Serialization.Json;
+
+public class JsonSerializerOptionsFactoryTests
+{
+  [Fact]
+  public void Create_ReturnsJsonSerializerOptions()
+  {
+    var o = JsonSerializerOptionsFactory.Create();
+
+    Assert.IsType<JsonSerializerOptions>(o);
+  }
+}
