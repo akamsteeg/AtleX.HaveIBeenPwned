aed5db8242c02c80657b098855195b18de1a40b6
Alex Kamsteeg <alex@atlex.nl>
2022-11-02 17:54:08 +0100

    Add a trait to the integration tests that require an API key

---
+++ src/AtleX.HaveIBeenPwned.IntegrationTests/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace AtleX.HaveIBeenPwned.IntegrationTests;
+public static class Constants
+{
+  public static class Tests
+  {
+    public static class Categories
+    {
+      public static class RequiresApiKeyCategory
+      {
+        public const string Name = "RequiresApiKey";
+      }
+    }
+  }
+}
