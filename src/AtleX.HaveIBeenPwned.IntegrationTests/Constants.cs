b80d281cac679fd09ddfe7f0bdab04ac0d512a0c
Alex Kamsteeg <alex@atlex.nl>
2023-05-11 20:25:13 +0200

    Updated documentation and wait time between inntegration tests

--- src/AtleX.HaveIBeenPwned.IntegrationTests/Constants.cs
+++ src/AtleX.HaveIBeenPwned.IntegrationTests/Constants.cs
@@ -9,14 +9,27 @@ internal static class Constants
     /// <summary>
     /// Gets the <see cref="TimeSpan"/> to wait at the end of each test to prevent hotting the rate limit
     /// </summary>
-    public static readonly TimeSpan DelayBetweenTests = 7.Seconds();
+    public static readonly TimeSpan DelayBetweenTests = 9.Seconds();
 
+    /// <summary>
+    /// Gets the name of the integration tests to use as the application name
+    /// when calling the HaveIBeenPwned.com API
+    /// </summary>
     public const string ApplicationName = "AtleX.HaveIBeenPwned.IntegrationTests";
 
+    /// <summary>
+    /// Represents the test categories used
+    /// </summary>
     public static class Categories
     {
+      /// <summary>
+      /// Represents a test category for tests that require an API key
+      /// </summary>
       public static class RequiresApiKeyCategory
       {
+        /// <summary>
+        /// Gets the name of the test category
+        /// </summary>
         public const string Name = "RequiresApiKey";
       }
     }
