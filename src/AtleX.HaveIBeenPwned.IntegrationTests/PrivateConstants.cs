6ed43b373f9dcff991c6e8cefac6c8881dc4cc27
Alex Kamsteeg <alex@atlex.nl>
2023-05-19 10:10:29 +0200

    Updated documentation

--- src/AtleX.HaveIBeenPwned.IntegrationTests/PrivateConstants.cs
+++ src/AtleX.HaveIBeenPwned.IntegrationTests/PrivateConstants.cs
@@ -4,5 +4,8 @@
 /// </summary>
 internal static class PrivateConstants
 {
+  /// <summary>
+  /// Gets the API key to use for integration tests that are requiring one
+  /// </summary>
   public const string ApiKey = "";
 }
