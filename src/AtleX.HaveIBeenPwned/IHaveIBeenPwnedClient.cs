e305b28b4be4094764afd79f2acf0a9f4f26c498
Alex Kamsteeg <alex@atlex.nl>
2023-05-24 17:57:53 +0200

    Code formatting

--- src/AtleX.HaveIBeenPwned/IHaveIBeenPwnedClient.cs
+++ src/AtleX.HaveIBeenPwned/IHaveIBeenPwnedClient.cs
@@ -1,7 +1,8 @@
 ﻿namespace AtleX.HaveIBeenPwned;
 
 /// <summary>
-/// Represents a client for the <see href="https://haveibeenpwned.com/">HaveIBeenPwned</see> service
+/// Represents a client for the <see
+/// href="https://haveibeenpwned.com/">HaveIBeenPwned</see> service
 /// </summary>
 public interface IHaveIBeenPwnedClient
   : IHaveIBeenPwnedBreachesClient, IHaveIBeenPwnedPastesClient, IHaveIBeenPwnedPasswordClient
