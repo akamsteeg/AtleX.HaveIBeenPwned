c901dac9c159db996e8e252f1ed47235bd857e77
Alex Kamsteeg <alex@atlex.nl>
2023-09-26 19:32:14 +0200

    Add suppresion of RCS1194 Implement exception constructors
    We really want to avoid inadvertently including domains in exception messages etc. The exception messages might end up in logging or shown to end users. We don't want to leak private or internal domains that way

--- src/AtleX.HaveIBeenPwned/DomainForbiddenException.cs
+++ src/AtleX.HaveIBeenPwned/DomainForbiddenException.cs
@@ -4,7 +4,12 @@
 /// Represents a <see cref="HaveIBeenPwnedClientException"/> for when the specified API key
 /// does not have access to the requested domain in the system
 /// </summary>
+#pragma warning disable RCS1194 // Implement exception constructors.
+// We really want to avoid inadvertently including domains in exception
+// messages etc. The exception messages might end up in logging or shown
+// to end users. We don't want to leak private or internal domains that way
 public sealed class DomainForbiddenException
+#pragma warning restore RCS1194 // Implement exception constructors.
   : HaveIBeenPwnedClientException
 {
 
