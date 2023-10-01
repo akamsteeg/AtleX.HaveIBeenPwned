07ff5e2ac0f3d1466ffbc8558f167fbbdbfa2944
Alex Kamsteeg <alex@atlex.nl>
2023-10-01 13:18:34 +0200

    Removed .NET Core 3.1 from the unit tests, integration tests and benchmarks (fixes #92)
    .NET Core 3.1 is out of support for, at the time of writing, almost 10 months already. It is no longer available on the build machines this project is using and newer versions of Visual Studio also skip runing for example the unit & integration tests for this target. It's time to drop it.

--- src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
+++ src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
@@ -33,7 +33,6 @@ public static class Program
 
     config.AddJob(
       Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp60).AsBaseline(),
-      Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp31),
       Job.Default.WithToolchain(CsProjClassicNetToolchain.Net481)
       );
 
