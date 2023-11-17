212afa832196445698437fecff42fb7b9e9206d8
Alex Kamsteeg <alex@atlex.nl>
2023-11-17 10:56:16 +0100

    Fix naming of tests to be consistent with the other tests

--- src/AtleX.HaveIBeenPwned.Tests/HaveIBeenPwnedClientTests_GetBreachedDomainUsersAsync.cs
+++ src/AtleX.HaveIBeenPwned.Tests/HaveIBeenPwnedClientTests_GetBreachedDomainUsersAsync.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace AtleX.HaveIBeenPwned.Tests;
-public class HaveIBeenPwnedClient_GetBreachedDomainUsersAsyncTests
+public class HaveIBeenPwnedClientTests_GetBreachedDomainUsersAsync
   : HaveIBeenPwnedClientTestsBase
 {
   [Fact]
