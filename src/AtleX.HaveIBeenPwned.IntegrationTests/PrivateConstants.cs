f2c5654d115db80ba9581812ac268fb2381c0e0a
Alex Kamsteeg <alex@atlex.nl>
2023-12-22 10:31:57 +0100

    Removed API key
    The old API key was also cycled so that it's still in the Git history is no problem.

--- src/AtleX.HaveIBeenPwned.IntegrationTests/PrivateConstants.cs
+++ src/AtleX.HaveIBeenPwned.IntegrationTests/PrivateConstants.cs
@@ -7,5 +7,5 @@ internal static class PrivateConstants
   /// <summary>
   /// Gets the API key to use for integration tests that are requiring one
   /// </summary>
-  public const string ApiKey = "e39534cfcf7644c0ab0e63840aa89696";
+  public const string ApiKey = "";
 }
