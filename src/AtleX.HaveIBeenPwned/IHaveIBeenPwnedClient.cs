2fd0e5229bc0942a5dd42ece8bce2462ea213a43
Alex Kamsteeg <alex@atlex.nl>
2024-02-02 13:06:40 +0100

    Improved documentation

--- src/AtleX.HaveIBeenPwned/IHaveIBeenPwnedClient.cs
+++ src/AtleX.HaveIBeenPwned/IHaveIBeenPwnedClient.cs
@@ -1,8 +1,14 @@
 ﻿namespace AtleX.HaveIBeenPwned;
 
 /// <summary>
-/// Represents a client for the <see
-/// href="https://haveibeenpwned.com/">HaveIBeenPwned</see> service
+/// <para>
+/// Represents a client for the <see href="https://haveibeenpwned.com/">HaveIBeenPwned</see> service
+/// </para>
+/// <para>
+/// See <see cref="IHaveIBeenPwnedBreachesClient"/>, <see cref="IHaveIBeenPwnedPastesClient"/>, <see
+/// cref="IHaveIBeenPwnedPasswordClient"/> or <see cref="IHaveIBeenPwnedDomainClient"/> for more
+/// specialized interfaces implemented by this interface.
+/// </para>
 /// </summary>
 public interface IHaveIBeenPwnedClient
   : IHaveIBeenPwnedBreachesClient,
