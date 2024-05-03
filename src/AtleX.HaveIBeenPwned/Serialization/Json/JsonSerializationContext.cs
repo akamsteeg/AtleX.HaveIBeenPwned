c5c8f34124085db4ab58c464e4b159f4b4577eda
Alex Kamsteeg <alex@atlex.nl>
2024-05-03 04:53:56 +0200

    Rolled back change that broke on .NET 8.0

--- src/AtleX.HaveIBeenPwned/Serialization/Json/JsonSerializationContext.cs
+++ src/AtleX.HaveIBeenPwned/Serialization/Json/JsonSerializationContext.cs
@@ -1,5 +1,6 @@
 ﻿#if NET6_0_OR_GREATER
 
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
@@ -8,9 +9,15 @@ namespace AtleX.HaveIBeenPwned.Serialization.Json;
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Default)]
 
 [JsonSerializable(typeof(Breach))]
+[JsonSerializable(typeof(IEnumerable<Breach>))]
+
 [JsonSerializable(typeof(Paste))]
+[JsonSerializable(typeof(IEnumerable<Paste>))]
+
 [JsonSerializable(typeof(SiteBreach))]
-[JsonSerializable(typeof(DomainUser))]
+[JsonSerializable(typeof(IEnumerable<SiteBreach>))]
+
+[JsonSerializable(typeof(IEnumerable<DomainUser>))] // For domain breaches
 
 [ExcludeFromCodeCoverage]
 internal sealed partial class JsonSerializationContext
