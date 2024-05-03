9a4bf53c91c155eb47ce91609df677063f3d6eb6
Alex Kamsteeg <alex@atlex.nl>
2024-05-03 05:58:20 +0200

    Added running benchmarks for NativeAOT

--- src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
+++ src/AtleX.HaveIBeenPwned.Benchmarks/Program.cs
@@ -6,6 +6,7 @@ using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.CsProj;
+using BenchmarkDotNet.Toolchains.NativeAot;
 using BenchmarkDotNet.Validators;
 using System;
 using System.Reflection;
@@ -27,8 +28,9 @@ public static class Program
     var config = ManualConfig.Create(DefaultConfig.Instance)
       .AddJob(
         Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp80).AsBaseline(),
+        Job.Default.WithToolchain(NativeAotToolchain.CreateBuilder().UseNuGet().IlcInstructionSet("native").ToToolchain()), // Must be changed to Job.Default.WithRuntime(NativeAotRuntime.Net80), when BenchmarkDotNet 0.14 is released
         Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp60)
-      )
+        )
       .AddDiagnoser(MemoryDiagnoser.Default);
 
     if (Environment.OSVersion.Platform == PlatformID.Win32NT)
